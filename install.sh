863b400e3c8dbbcea99a54225c758cf8df1af02e
James Hobson <james@localhost.localdomain>
2017-12-29 02:06:35 +0000

    Fix2

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-cp -a i3 ~/.config
-cp -a polybar ~/.config
-cp compton.conf ~/.config
+cp -a config/i3 ~/.config
+cp -a config/polybar ~/.config
+cp config/compton.conf ~/.config
 
 sudo zypper in i3-gaps rofi compton i3-gaps-devel libcairo2 cairo-devel python2-cairo python2-cairo-devel cmake patterns-devel-base-devel_basis patterns-devel-C-C++-devel_C_C++ patterns-yast-x11_yast patterns-fonts-fonts patterns-fonts-fonts_opt patterns-base-enhanced_base patterns-base-x11 patterns-base-x11_opt libxcb-devel python xcb-proto-devel xcb-util-image-devel xcb-util-wm-devel xcb-util-xrm-devel rxvt-unicode nitrogen
 
