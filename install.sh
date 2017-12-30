e41c4e4f7229149e95ea9daa3492d8795089b77c
James Hobson <james@localhost.localdomain>
2017-12-30 16:04:35 +0000

    Added dependancies for internet and mpd module, added cpu, ethernet and wifi to polybar

--- install.sh
+++ install.sh
@@ -4,10 +4,15 @@ cp config/compton.conf ~/.config
 cp config/.extend.Xresources ~/
 cp config/.Xresources ~/ 
 
-sudo zypper in i3-gaps rofi compton i3-gaps-devel libcairo2 cairo-devel python2-cairo python2-cairo-devel cmake patterns-devel-base-devel_basis patterns-devel-C-C++-devel_C_C++ patterns-yast-x11_yast patterns-fonts-fonts patterns-fonts-fonts_opt patterns-base-enhanced_base patterns-base-x11 patterns-base-x11_opt libxcb-devel python xcb-proto-devel xcb-util-image-devel xcb-util-wm-devel xcb-util-xrm-devel rxvt-unicode nitrogen psmisc
+sudo zypper in i3-gaps rofi compton i3-gaps-devel libcairo2 cairo-devel python2-cairo python2-cairo-devel cmake patterns-devel-base-devel_basis patterns-devel-C-C++-devel_C_C++ patterns-yast-x11_yast patterns-fonts-fonts patterns-fonts-fonts_opt patterns-base-enhanced_base patterns-base-x11 patterns-base-x11_opt libxcb-devel python xcb-proto-devel xcb-util-image-devel xcb-util-wm-devel xcb-util-xrm-devel rxvt-unicode nitrogen psmisc jsoncpp-devel libxcb-cursor0 alsa-devel libmpd-devel libcurl-devel wireless-tools libiw-devel libmpdclient-devel
 
-rm -rf polybar
-git clone --branch 3.1.0 --recursive https://github.com/jaagr/polybar
+killall -q polybar
+
+if [ ! -d "polybar" ]; then
+  git clone --branch 3.1.0 --recursive https://github.com/jaagr/polybar
+else
+  sudo rm -rf polybar/build
+fi
 mkdir polybar/build
 cd polybar/build
 cmake ..
