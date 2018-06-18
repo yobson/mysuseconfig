eb055334d66fd981cad7727b54743db48f779ba9
James Hobson <james.hobson@oriel.ox.ac.uk>
2018-06-18 20:05:43 +0100

    Added new icon theme

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cp config/.extend.Xresources ~/
 cp config/.Xresources ~/
 cp config/.xinitrc ~/
 
-sudo zypper in i3-gaps rofi compton i3-gaps-devel libcairo2 cairo-devel python2-cairo python2-cairo-devel cmake patterns-devel-base-devel_basis patterns-devel-C-C++-devel_C_C++ patterns-yast-x11_yast patterns-fonts-fonts patterns-fonts-fonts_opt patterns-base-enhanced_base patterns-base-x11 patterns-base-x11_opt libxcb-devel python xcb-proto-devel xcb-util-image-devel xcb-util-wm-devel xcb-util-xrm-devel rxvt-unicode nitrogen psmisc jsoncpp-devel libxcb-cursor0 alsa-devel libmpd-devel libcurl-devel wireless-tools libiw-devel libmpdclient-devel gcc gcc-cpp
+sudo zypper in i3-gaps rofi compton i3-gaps-devel libcairo2 cairo-devel python2-cairo python2-cairo-devel cmake patterns-devel-base-devel_basis patterns-devel-C-C++-devel_C_C++ patterns-yast-x11_yast patterns-fonts-fonts patterns-fonts-fonts_opt patterns-base-enhanced_base patterns-base-x11 patterns-base-x11_opt libxcb-devel python xcb-proto-devel xcb-util-image-devel xcb-util-wm-devel xcb-util-xrm-devel rxvt-unicode nitrogen psmisc jsoncpp-devel libxcb-cursor0 alsa-devel libmpd-devel libcurl-devel wireless-tools libiw-devel libmpdclient-devel gcc gcc-cpp lxqt-sudo lxappearence
 
 killall -q polybar
 
@@ -25,3 +25,7 @@ cd ~/Backdrops
 curl -O https://raw.githubusercontent.com/unix121/i3wm-themer/master/themes/Minimal/Minimal.png
 nitrogen --set-zoom-fill ~/Backdrops/Minimal.png
 
+git clone https://github.com/pop-os/icon-theme
+cd icon-theme
+sudo make install
+sudo make post-install
